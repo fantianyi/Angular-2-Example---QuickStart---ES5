bb217e9da4377a5e38559af2641d44396a41c571
fantianyi <fty7809@gmail.com>
2016-08-13 11:18:46 +0800

    Update to angular2 2.0.0-rc.5 Use ng.core.NgModule

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1><div>Hello World!</div>'
+      template: '<h1>My First Angular 2 App</h1><div>with ES5 and angular2 2.0.0.rc.5!</div>'
     })
     .Class({
       constructor: function() {}
